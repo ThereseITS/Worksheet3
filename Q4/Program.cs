9b4ab58fac7c5e769d68a2dfd81521a868356c3a
Therese Hume <Therese.Hume@atu.ie>
2023-02-22 14:22:55 +0000

    q4 re-added

---
+++ Q4/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace Q4
+{
+    using static System.Console;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] words = { "", "rortor", "abba", "absba", "q" };// test a few combinations
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (IsPalindrome(words[i]))
+                {
+
+                    WriteLine(words[i] + " is a palindrome");
+                }
+                else
+                {
+                    WriteLine(words[i] + " is not a palindrome");
+                }
+
+            }
+            ReadLine();
+
+        }
+        static bool IsPalindrome(string word)
+        {
+            bool pal = true;
+
+            if (word == null)
+            {
+                return false;
+            }
+
+            int j = word.Length - 1;
+
+            for (int i = 0; i < word.Length / 2; i++)
+
+            {
+
+                if (word[i] != word[j])
+                {
+                    pal = false;
+                }
+                j--;
+            }
+            return pal;
+        }
+    }
+}
